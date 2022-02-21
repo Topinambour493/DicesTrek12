37a5a25b3d111fa9561ef81b33a14857f32f0329
corentin jezierski <corentin.jezierski@gmail.com>
2022-02-21 14:19:53 +0100

    mise en place des ; sur le code js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //cette fonction renvoit un nombre random entre 0 et i exclus
 function get_random_number(i){
-    return Math.floor(Math.random() *i)
+    return Math.floor(Math.random() *i);
 }
 
 function roll_dices(){
-    document.getElementById("previous_dice1").innerHTML = document.getElementById("dice1").innerHTML
-    document.getElementById("previous_dice2").innerHTML = document.getElementById("dice2").innerHTML
-    document.getElementById("dice1").innerHTML = `${1+get_random_number(6)}`
-    document.getElementById("dice2").innerHTML = `${get_random_number(6)}`
+    document.getElementById("previous_dice1").innerHTML = document.getElementById("dice1").innerHTML;
+    document.getElementById("previous_dice2").innerHTML = document.getElementById("dice2").innerHTML;
+    document.getElementById("dice1").innerHTML = `${1+get_random_number(6)}`;
+    document.getElementById("dice2").innerHTML = `${get_random_number(6)}`;
 }
 
 document.getElementById("roll").addEventListener("click",roll_dices);
