3593511df130f761a6f58e607990d268daef5d30
corentin jezierski <corentin.jezierski@gmail.com>
2022-02-21 14:09:46 +0100

    ajout des dés précédents qui permet de voir quand il y a deux fois la meme combinaison avec certitude

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function get_random_number(i){
 }
 
 function roll_dices(){
+    document.getElementById("previous_dice1").innerHTML = document.getElementById("dice1").innerHTML
+    document.getElementById("previous_dice2").innerHTML = document.getElementById("dice2").innerHTML
     document.getElementById("dice1").innerHTML = `${1+get_random_number(6)}`
     document.getElementById("dice2").innerHTML = `${get_random_number(6)}`
 }
